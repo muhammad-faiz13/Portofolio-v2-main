940aff72d4c6772bc196e95d6eace32b49c77a80
Muhammad faiz <faiz6677889@gmail.com>
2025-07-15 13:13:01 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue-cli-service')
 module.exports = {
        transpileDependencies: [
            'dependency-name'
-       ]
+       ],
+  publicPath: process.env.NODE_ENV === "production" ? "/Portofolio-v2-main/" : "/",
    };
+
+
